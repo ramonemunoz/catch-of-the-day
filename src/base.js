30613129fa7390e313cee2bfc50015775743e447
ramonemunoz <ramonemunoz@user.noreply.github.com>
2017-12-07 23:45:42 -0800

    Up to video 18

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: 'AIzaSyCQUFB_UVUmAEmkU40J9RS-VDDUFzf481k',
+	authDomain: 'catch-of-the-day-b5489.firebaseapp.com',
+	databaseURL: 'https://catch-of-the-day-b5489.firebaseio.com'
+});
+
+export default base;
